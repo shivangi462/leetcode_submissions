592a365ea1b69d53f8637494fa886bc1826085ce
shivangi_singh <93123654+shivangi462@users.noreply.github.com>
2023-07-27 09:24:31 +0530

    Time: 53 ms (61.97%) | Memory: 18.9 MB (37.54%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -3,15 +3,8 @@ public:
     bool ok(vector<int>& piles,int mid,int h){
     int count=0;
     for(int i=0;i<piles.size();++i){
-        double temp=(double)piles[i]/(double)mid;
-        
-        if(temp>(int)temp){
-            count+=(int)temp+1;
-        }
-        else{
-            count+=temp;
-        }
-       
+        int temp=ceil((double)piles[i]/(double)mid);
+        count+=temp;
     }
     if(count>h){
            return false;
