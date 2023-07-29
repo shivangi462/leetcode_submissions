2800f64d564f3f16a7b0a5093aceb4f61c9286d3
shivangi_singh <93123654+shivangi462@users.noreply.github.com>
2023-07-29 12:32:03 +0530

    Time: 23 ms (92.48%) | Memory: 22.3 MB (74.94%) - LeetSync

---
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+   
+    int singleNonDuplicate(vector<int>& nums) {
+        // int lmt = nums.size() - 1, mid;
+        // // binary search
+        // for (int l = 0, r = lmt, prev, nxt; l <= r;) {
+        //     // updating loop variables
+        //     mid = (l + r) >> 1;
+        //     prev = mid ? nums[mid - 1] : nums[mid] - 1;
+        //     nxt = mid != lmt ? nums[mid + 1] : nums[mid] + 1;
+        //     // checking the element pointed by mid:
+        //     // match
+        //     if (nums[mid] != prev && nums[mid] != nxt) return nums[mid];
+        //     // too high
+        //     else if (nums[mid] != (mid & 1 ? prev : nxt)) r = mid - 1;
+        //     // too low
+        //     else l = mid + 1;
+        // }
+        // return nums[mid];
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+          sum^=nums[i];
+        }
+        return sum;
+    }
+};
