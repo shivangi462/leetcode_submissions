d8d7f51f004795c402df665156cdc2a6a11a05da
shivangi_singh <93123654+shivangi462@users.noreply.github.com>
2023-07-29 13:23:57 +0530

    Time: 18 ms (98.85%) | Memory: 22.3 MB (74.94%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -2,26 +2,52 @@ class Solution {
 public:
    
     int singleNonDuplicate(vector<int>& nums) {
-        // int lmt = nums.size() - 1, mid;
-        // // binary search
-        // for (int l = 0, r = lmt, prev, nxt; l <= r;) {
-        //     // updating loop variables
-        //     mid = (l + r) >> 1;
-        //     prev = mid ? nums[mid - 1] : nums[mid] - 1;
-        //     nxt = mid != lmt ? nums[mid + 1] : nums[mid] + 1;
-        //     // checking the element pointed by mid:
-        //     // match
-        //     if (nums[mid] != prev && nums[mid] != nxt) return nums[mid];
-        //     // too high
-        //     else if (nums[mid] != (mid & 1 ? prev : nxt)) r = mid - 1;
-        //     // too low
-        //     else l = mid + 1;
+        
+        // int sum=0;
+        // for(int i=0;i<nums.size();i++){
+        //   sum^=nums[i];
         // }
-        // return nums[mid];
-        int sum=0;
-        for(int i=0;i<nums.size();i++){
-          sum^=nums[i];
-        }
-        return sum;
+        // return sum;
+
+        // 1 1 2 3 3 4 4 8 8
+        // 0 1 2 3 4 5 6 7 8
+
+        //[3,3,7,7,10,11,11]
+        // 0 1 2 3 4  5   6
+
+        // l=0 r=8 mid=4   4 is even mtlb ki single element occurence ke baad 
+        //                 ka pattern(nums[i]==nums[i+1] iska true hona mtlb ki  
+        //                 answer left me hai )toh l=mid+1;
+        //                 wrna r=mid;    yha cond false hai isliye r=mid;
+
+        // l=0 r=4 mid=2   2 is even mtlb ki single element occurence ke baad 
+        //                 ka pattern(nums[i]==nums[i+1] iska true hona mtlb ki  
+        //                 answer left me hai )toh l=mid+1;
+        //                 wrna r=mid;    yha cond false hai isliye r=mid;            
+
+        // l=0 r=2 mid=1   1 is odd mtlb ki single element occurence ke phle 
+        //                 ka pattern(nums[i]==nums[i-1] iska true hona mtlb ki  
+        //                 answer right me hai )toh l=mid+1;
+        //                 wrna r=mid;     yha cond true hai isliye l=mid+1;
+        // l=2 r=2 jb l==r barabar hogye toh whi hamara answer hai     
+
+
+
+
+        int n=nums.size();
+        int l=0,r=n-1;
+
+        while(l<r){
+
+          int mid=(l+r)/2;
+
+          if((mid%2==0 && nums[mid]==nums[mid+1]) || (mid%2==1 && nums[mid]==nums[mid-1])){
+            l=mid+1;
+          }
+          else{
+            r=mid;
+          }
+        } 
+        return nums[l];          
     }
 };
