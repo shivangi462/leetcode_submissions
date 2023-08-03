9fbdc9306b99526475e0af11a26635d8a34360e5
shivangi_singh <93123654+shivangi462@users.noreply.github.com>
2023-08-03 20:52:51 +0530

    Time: 160 ms (96.41%) | Memory: 66.5 MB (62.82%) - LeetSync

--- 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.cpp
+++ 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
     int minSubarray(vector<int>& nums, int p) {
-      int n=nums.size();
-        long long t_sum=0;
+        int n=nums.size();
+
+        long long totalSum=0;
         for(int i=0;i<n;i++){
-          t_sum+=nums[i];
+          totalSum+=nums[i];
         }
 
-        
-        int t_rem=t_sum%p;
-        if(t_rem==0)return 0;
-        // cout<<t_sum;
-        unordered_map<int,int> mp;  // rem,index
+        int remainder1=totalSum%p;
+        if(remainder1==0)return 0;
+        unordered_map<int,int> mp;  // index_rem,index
         int size=INT_MAX;
         long long sum=0;
         for(int i=0;i<n;i++){
           sum+=nums[i];
-           int rem=sum%p;
-           if(rem==t_rem){
+           int index_rem=sum%p;
+           if(index_rem==remainder1){
              size=min(size,i+1);
            }
-           if(mp.find(rem - t_rem)!=mp.end()){
-             size=min(size,i-mp[rem-t_rem]);
+           if(mp.find(index_rem - remainder1)!=mp.end()){
+             size=min(size,i-mp[index_rem-remainder1]);
            }
-           if(mp.find(p+rem-t_rem)!=mp.end()){
-              size=min(size,i-mp[p+rem-t_rem]);
+           if(mp.find(p+index_rem-remainder1)!=mp.end()){
+              size=min(size,i-mp[p+index_rem-remainder1]);
            }
-           mp[rem]=i;
+           mp[index_rem]=i;
         }
         if(size>=nums.size()) return -1;
         return size;
