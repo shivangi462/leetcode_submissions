7c0daba6c3aadea31ab1e49fff59c1f8d487fc02
shivangi_singh <93123654+shivangi462@users.noreply.github.com>
2023-08-15 12:06:19 +0530

    Time: 7 ms (47.09%) | Memory: 10.5 MB (9.03%) - LeetSync

--- 86-partition-list/partition-list.cpp
+++ 86-partition-list/partition-list.cpp
@@ -10,7 +10,9 @@
  */
 class Solution {
 public:
-    void insert(ListNode** root, int item)
+
+
+    void insert(ListNode** root, int item)  // 
 {
     ListNode* temp = new ListNode(item);
     ListNode* ptr;
@@ -30,17 +32,24 @@ public:
         ptr->next = temp;
     }
 }
+
+
     ListNode* partition(ListNode* head, int x) {
-        vector<int> arr;
-        vector<int> arr2;
-        ListNode* dhead=head;
+      // 1 4 3 2 5 2 
+        vector<int> arr; // wo values store karega jo x se kam hongi
+        vector<int> arr2; // wo values store karega jo x ke barabar aur x se badi values
+
+        
         while(head!=nullptr){
-          if(head->val<x) arr.push_back(head->val);
-          else arr2.push_back(head->val);
+          if(head->val < x) 
+             arr.push_back(head->val);
+          else 
+             arr2.push_back(head->val);
+
           head=head->next;
         }
-        // for(int i=0;i<arr.size();i++)
-        // cout<<arr[i];
+        // arr -> 1 2 2
+        // arr2 -> 4 3 5 
         ListNode* ans=NULL ;
 
         for(int i=0;i<arr.size();i++){
