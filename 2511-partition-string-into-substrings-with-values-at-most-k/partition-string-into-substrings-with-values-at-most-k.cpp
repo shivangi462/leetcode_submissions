8da34fcd80e758c1778132dead7013bc82898eb0
shivangi_singh <93123654+shivangi462@users.noreply.github.com>
2023-08-23 21:13:02 +0530

    Time: 32 ms (39.16%) | Memory: 8.9 MB (69.88%) - LeetSync

---
+++ 2511-partition-string-into-substrings-with-values-at-most-k/partition-string-into-substrings-with-values-at-most-k.cpp
@@ -0,0 +1,45 @@
+#define ll long long 
+class Solution {
+public:
+    int minimumPartition(string s, int k) {
+        string temp=""; ll cnt=0; 
+ 
+    for(int i=0; i<s.size(); i++) 
+    {
+        temp+=s[i];
+ 
+        ll val = stoll(temp);
+ 
+        if(val<=k) 
+        {
+            // do nothing
+        }
+        else
+        {
+            cnt++;
+            temp=s[i]; 
+ 
+            if((s[i]-'0')>k) {
+               return -1;
+            }
+        }
+    }
+ 
+    if(temp!="") 
+    {
+         ll val = stoll(temp);
+ 
+         if(val<=k) 
+        {
+           cnt++;
+        }
+        else
+        {
+           cout<<-1<<" ";  
+                return 0;  
+        }
+    }
+ 
+      return cnt;
+    }
+};
