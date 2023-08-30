b07fb08aa15572b8a1bbe736b25e478d5a29027e
shivangi_singh <93123654+shivangi462@users.noreply.github.com>
2023-08-30 16:32:08 +0530

    Time: 2272 ms (5.01%) | Memory: 505.8 MB (5.00%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -10,7 +10,12 @@ public:
         //  1 3
         //  3 4
         int l=intervals[0][0],r=intervals[0][1],n=intervals.size(),ans=1;
-        for (int i=1;i<n;i++) if (intervals[i][0]>=r) {ans++;r=intervals[i][1];}
+        for (int i=1;i<n;i++) {
+          if (intervals[i][0]>=r) {
+            ans++;
+            r=intervals[i][1];
+          }
+        }
         return n-ans;
     }
 };
