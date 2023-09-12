a6ec09a2db63020c5eafa5f1ca9ba9fbac69db3d
shivangi_singh <93123654+shivangi462@users.noreply.github.com>
2023-09-12 11:42:07 +0530

    Time: 29 ms (43.81%) | Memory: 9 MB (44.56%) - LeetSync

--- 2511-partition-string-into-substrings-with-values-at-most-k/partition-string-into-substrings-with-values-at-most-k.cpp
+++ 2511-partition-string-into-substrings-with-values-at-most-k/partition-string-into-substrings-with-values-at-most-k.cpp
@@ -2,43 +2,30 @@
 class Solution {
 public:
     int minimumPartition(string s, int k) {
-        string temp=""; ll cnt=0; 
- 
-    for(int i=0; i<s.size(); i++) 
-    {
-        temp+=s[i];
- 
-        ll val = stoll(temp);
- 
-        if(val<=k) 
-        {
-            // do nothing
-        }
-        else
-        {
-            cnt++;
-            temp=s[i]; 
- 
-            if((s[i]-'0')>k) {
-               return -1;
+       int partition=0;
+       string temp="";
+       int size=s.size();
+       for(int i=0;i<size;i++){
+            temp+=s[i];
+
+            ll val=stoll(temp);
+
+            if(val<=k) {// do nothing
             }
-        }
-    }
- 
-    if(temp!="") 
-    {
-         ll val = stoll(temp);
- 
-         if(val<=k) 
-        {
-           cnt++;
-        }
-        else
-        {
-           return -1;  
-        }
-    }
- 
-      return cnt;
+            else{
+              partition++;
+              temp=s[i];
+
+              if(s[i]-'0' > k) return -1;
+            }
+       }
+
+       if(temp!=""){
+            ll val=stoll(temp);
+
+            if(val<=k) partition++;
+            else return -1; 
+       }
+       return partition;
     }
 };
