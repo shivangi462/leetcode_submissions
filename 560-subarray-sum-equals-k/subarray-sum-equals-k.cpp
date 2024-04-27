d42d5fe9e6d2cb18372307686bfe465257eb3808
shivangi_singh <93123654+shivangi462@users.noreply.github.com>
2024-04-27 10:41:12 +0530

    Time: 91 ms (39.78%) | Memory: 45.1 MB (57.62%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -3,14 +3,26 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         int ans=0;
         int n=nums.size();
-        int sum;
+        int prefix[n];
+        prefix[0]=nums[0];
+
+        for(int i=1;i<n;i++){
+            prefix[i]=prefix[i-1]+nums[i];
+        }
         for(int i=0;i<n;i++){
-          sum=nums[i];
-          if(sum==k) ans++;
-          for(int j=i+1;j<n;j++){
-             sum+=nums[j];
-             if(sum==k) ans++;
-          } 
+            cout<<prefix[i];
+        }
+
+        unordered_map<int,int> mp;
+
+        for(int i=0;i<n;i++){
+            if(prefix[i]==k) ans++;
+
+            if(mp.find(prefix[i]-k)!=mp.end()){
+                ans+=mp[prefix[i]-k];
+            }
+
+            mp[prefix[i]]++;
         }
         return ans;
     }
